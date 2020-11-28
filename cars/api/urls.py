4fc06167ba68125c470549ebd71c97b0e9bd2f38
Mathieu Robichaud <admin@birkoss.com>
2020-11-28 12:16:08 -0500

    Added an API prefix for the urls

--- cars/api/urls.py
+++ cars/api/urls.py
@@ -5,17 +5,17 @@ from . import views as api_views
 
 urlpatterns = [
     path(
-        'makes',
+        'api/makes',
         api_views.makes.as_view(),
         name='makes'
     ),
     path(
-        'make/<str:make>/models',
+        'api/make/<str:make>/models',
         api_views.models.as_view(),
         name='models'
     ),
     path(
-        'make/<str:make>/model/<str:model>/trims',
+        'api/make/<str:make>/model/<str:model>/trims',
         api_views.trims.as_view(),
         name='trims'
     ),
