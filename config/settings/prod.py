4f9ba831555ff9f86b7f2d317983c1e08fb31031
Mathieu Robichaud <admin@birkoss.com>
2020-11-28 07:47:52 -0500

    Initial Commit

---
+++ config/settings/prod.py
@@ -0,0 +1,23 @@
+from .base import *
+
+
+ALLOWED_HOSTS = ['']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': get_secret('DATABASE_NAME'),
+        'USER': get_secret('DATABASE_USER'),
+        'PASSWORD': get_secret('DATABASE_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+    }
+}
+
+
+STATIC_URL = 'https://ABC/assets/api/'
+STATIC_ROOT = '/home/ABC/public_html/assets/api/'
